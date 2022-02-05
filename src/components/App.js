b41e73b93f1f9ceb4e9955bf1a82cbf519f3b02a
Derek Chatham <Derek.ChathamSr@outlook.com>
2022-02-05 12:49:49 -0600

    new changes fixed issues

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 
-// import { Products, SingleProduct } from './';
+import { Products, SingleProduct } from './';
 
-// import { SingleProduct, Products } from './';
+import { SingleProduct, Products } from './';
 // getAPIHealth is defined in our axios-services directory index.js
 // you can think of that directory as a collection of api adapters
 // where each adapter fetches specific info from our express server's /api route
