a2c26653ce1b23eca23a6fa5d5117e87275cb4be
Derek Chatham <Derek.ChathamSr@outlook.com>
2022-02-05 12:39:03 -0600

    Changed Product to SingleProduct

--- src/components/styles.js
+++ src/components/styles.js
@@ -1,12 +1,12 @@
-import { makeStyles } from '@material-ui/core/styles';
+// import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles(() => ({
-    root: {
-        maxWidth: '100%'
-    },
-    media: {
-        height: 0,
-        paddingTop: 56.25%,
-    },
+// export default makeStyles(() => ({
+//     root: {
+//         maxWidth: '100%'
+//     },
+//     media: {
+//         height: 0,
+//         paddingTop: 56.25%
+//     },
     
-    }));
+//     }));
