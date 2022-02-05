0db2334bf1a9f5b713b2f7e9b58ccd29138e089f
Derek Chatham <Derek.ChathamSr@outlook.com>
2022-02-05 12:39:20 -0600

    New Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Products, SingleProduct } from './';
 // getAPIHealth is defined in our axios-services directory index.js
 // you can think of that directory as a collection of api adapters
 // where each adapter fetches specific info from our express server's /api route
@@ -24,7 +25,7 @@ const App = () => {
 
   return (
     <div className="app-container">
-      <h1>Hello, World!</h1>
+      <Products />
       <p>API Status: {APIHealth}</p>
     </div>
   );
--- src/components/Products.js
+++ src/components/Products.js
@@ -6,12 +6,13 @@ const products = [
     { id: 2, name: 'Glitter Sparkle Bomb NFT', description: 'Fake special made glitter bomb NFT '},
 ];
 
-const Products = () => {
+export const Products = () => {
     <main>
+        <h1>hello</h1>
         <Grid container justify='center' spacing={4}>
             {products.map((product) => {
                 <Grid item key = {product.id} xs={12} sm={6} md={4} lg = {3}>
-                    <Product />
+                    <SingleProduct />
                     </Grid>
             })}
         </Grid>
