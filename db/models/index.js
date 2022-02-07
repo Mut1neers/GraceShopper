3ad3c8552786df706ade08b525a0e90909b2790f
James <jcbarnes504@gmail.com>
2022-02-06 20:49:41 -0600

    modifed and tested api routes for products and productsID

--- db/models/index.js
+++ db/models/index.js
@@ -3,7 +3,7 @@ module.exports = {
   // so that you can use them in your express server api routers
   // for example, create a users.js file for a User model
   // and User: require('./user') here
-
+...require ("./products")
 
   
 };
