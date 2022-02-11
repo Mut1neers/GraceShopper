466e710a259a6f4fbfca05e4d3052e8f82ec4866
Caleb <cpford13@gmail.com>
2022-02-10 20:07:27 -0600

    Updated Heroku Database URL

--- db/client.js
+++ db/client.js
@@ -1,13 +1,15 @@
 // Connect to DB
-require('dotenv').config();
-const { Client } = require('pg');
+require("dotenv").config();
+const { Client } = require("pg");
 
 const { DB_PORT } = process.env || 5432;
 
 // change the DB_NAME string to whatever your group decides on
-const DB_NAME = 'rightClicked-dev';
+const DB_NAME = "rightClicked-dev";
 
-const DB_URL = process.env.DATABASE_URL || `postgres://localhost:${DB_PORT}/${DB_NAME}`;
+const DB_URL =
+  process.env.HEROKU_POSTGRESQL_CRIMSON_URL ||
+  `postgres://localhost:${DB_PORT}/${DB_NAME}`;
 
 const client = new Client(DB_URL);
 
