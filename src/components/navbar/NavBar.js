e8f29015a646f7b18a4369049b9f35b09f1ad4c0
Caleb <cpford13@gmail.com>
2022-02-14 19:11:46 -0600

    dealt with bullshit

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,33 +1,41 @@
-import React from 'react';
-import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
-import { ShoppingCart } from '@material-ui/icons';
-import useStyles from './navbarStyles';
-// import { mergeClasses } from '@material-ui/styles';
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  MenuItem,
+  Menu,
+  Typography,
+} from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
+import useStyles from "./navbarStyles";
+import { mergeClasses } from "@material-ui/styles";
 
 // import logo from '../assets/logo.png'
 
 const NavBar = () => {
-    const classes = useStyles();
-   return (
-       <>
-            <AppBar position='fixed' className={classes.appbar} color='inherit'>
-                <Toolbar>
-                    <Typography variatnt='h6' className={classes.title} color='inherit'>
-                       {/* // <img src={logo} atl='rightClicked' height='45px' className={classes.image}/> */}
-                        <h1>rightClicked Marketplace</h1>
-                    </Typography>
-                    <div className={classes.grow} />
-                    <div className={classes.button}>
-                        <IconButton aria-label='Show Cart Items' color='inherit'>
-                            <Badge badgeContent = {2} color='inherit'>
-                                <ShoppingCart />
-                            </Badge>
-                        </IconButton>
-                    </div>
-                </Toolbar>
-            </AppBar>
-       </>
-   ) 
-}
+  const classes = useStyles();
+  return (
+    <>
+      <AppBar position="fixed" className={classes.appbar} color="inherit">
+        <Toolbar>
+          <Typography variatnt="h6" className={classes.title} color="inherit">
+            {/* // <img src={logo} atl='rightClicked' height='45px' className={classes.image}/> */}
+            <h1>rightClicked Marketplace</h1>
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.button}>
+            <IconButton aria-label="Show Cart Items" color="inherit">
+              <Badge badgeContent={2} color="inherit">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
 
 export default NavBar;
