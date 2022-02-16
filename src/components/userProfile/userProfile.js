c26c1e1515197b9a492ad267bc3566f327235bde
Jack <jackmac776@gmail.com>
2022-02-16 11:44:28 -0600

    made changes to site.js to pull users data from api. It was lost when I pushed yesterday it semms.

--- src/components/userProfile/userProfile.js
+++ src/components/userProfile/userProfile.js
@@ -2,15 +2,22 @@ import React from 'react'
 import { useParams, Link, useHistory } from "react-router-dom";
 import { callApi } from "../../api";
 
-const UserProfile = ({userData, token}) => {
+const UserProfile = ({userData, token, users}) => {
 
     const History = useHistory()
     const { userId } = useParams();
+    const user = users.find((user) => parseInt(userId) === user.id)
     const isUser = userData.username === token.username
 
-    return (
-        <h2>Hello {userData.username}</h2>
-    )
+    return ( <div>
+        {user ? (
+            <h2>Hello {user.username}</h2>
+        ) : (
+            <h2> no User Here </h2>
+        )}
+        
+        </div>
+        )
 
 }
 
