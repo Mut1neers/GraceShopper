25d831520ab14678dacc95242dc5ff7060d799f2
austy <austincheckshismail@gmail.com>
2022-02-19 15:50:24 -0600

    fixed Login

--- src/components/site.js
+++ src/components/site.js
@@ -15,41 +15,21 @@ const Site = ({ products, setToken, userData, token, users, orders }) => {
         <Route exact path='/products/:productId'>
           <SingleProductPage products={products} />
         </Route>
-        {/* <Route exact path="/login">
-                <AccountForm
-                    action='login'
-                    setToken={setToken}
-                />
-            </Route>
-            <Route exact path="/register">
-                <AccountForm
-                    action='register'
-                    setToken={setToken}
-                />
-            </Route>
-            <Route exact path="/users/me">
-                <UserProfile
-                    userData={userData}
-                    token={token}
-                    users={users}
-                    orders={orders}
-                    action='myAccount'
-            />
-            </Route>
-            <Route exact path="/cart">
-                <Cart
-                    userData={userData}
-                    token={token}
-                    users={users}
-                    orders={orders}
-                />
-            </Route>
-        
-            </Switch>
-        </BrowserRouter>
-        
-
-    )
-}
+        <Route exact path='/login'>
+          <Login action='login' setToken={setToken} />
+        </Route>
+        <Route exact path='/register'>
+          <AccountForm action='register' setToken={setToken} />
+        </Route>
+        <Route exact path='/users/me'>
+          <UserProfile userData={userData} token={token} users={users} orders={orders} action='myAccount' />
+        </Route>
+        <Route exact path='/cart'>
+          <Cart userData={userData} token={token} users={users} orders={orders} />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default Site;
