d1f68ff556d4faa10735d267d2f24d0ddd853cde
Derek Chatham <Derek.ChathamSr@outlook.com>
2022-02-22 19:38:42 -0600

    finalizing some styling, fixing some errors

--- src/components/Home.js
+++ src/components/Home.js
@@ -21,11 +21,6 @@ const Home = () => {
           <Grid className='nftPng' item sm={6}>
             <img src={nft} alt='nfl logo' />
           </Grid>
-          <Grid className='info'>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </p>
-          </Grid>
         </Grid>
       </Container>
     </div>
