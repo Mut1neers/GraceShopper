2c716d9b9097d75b87f49f20a54bc3634080f1dc
Jack <jackmac776@gmail.com>
2022-02-23 16:46:21 -0600

    site works ??

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -4,47 +4,33 @@ import { SingleProduct } from "..";
 import { Link, useHistory, useParams } from "react-router-dom";
 import "../../style/products.css";
 
-const searchedProducts = (product, searchTerm) => {
-  const lowerCaseSearch = searchTerm.toLowerCase();
-  const { price, category, title, description } = product;
 
 
 
+ const Products = ({products, cart, setCart, addToCart}) => {
 
-  const toMatch = [description, category, title, price];
+      const history = useHistory();
+  return ( 
+   <main>
+       <div className='title'>Browse Through Tons of Fake NFTS</div>
+        
+        <Grid container justify='center' spacing={4}>
+            {products.map((product) => {
+                return (
+                   
+                    <SingleProduct 
 
-  for (const field of toMatch) {
-    if (field.toLowerCase().includes(lowerCaseSearch)) {
-      return true;
-    }
-  }
-};
-
-const Products = ({ products, cart, setCart }) => {
-  const [searchTerm, setSearchTerm] = useState("");
-  const history = useHistory();
-  const productsToDisplay = products.filter((product) =>
-    searchedProducts(product, searchTerm)
-  );
-
-  return (
-    <main>
-      <div className="title">Browse Through Tons of Fake NFTS</div>
-
-      <Grid container justify="center" spacing={4}>
-        {products.map((product) => {
-          return (
-            <Link to={`/products/${product.id}`}>
-              <SingleProduct
-                key={product.id}
-                product={product}
-                cart={cart}
-                setCart={setCart}
-              />
-            </Link>
-          );
-        })}
-      </Grid>
+                        key={product.id}
+                        product={product}
+                        cart={cart}
+                        setCart={setCart}
+                        addToCart={addToCart}
+                    />
+                    
+                )
+            
+            })}
+        </Grid>
     </main>
   );
 };
--- src/components/site.js
+++ src/components/site.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -18,16 +18,32 @@ import {
   Footer
 } from "./";
 
-const Site = ({
-  products,
-  setToken,
-  userData,
-  token,
-  users,
-  orders,
-  cart,
-  setCart,
-}) => {
+const Site = ({ products, setToken, userData, token, users, orders }) => {
+    const [cart, setCart] = useState([])
+    const addToCart = (product) => {
+        
+        console.log("product Clicked", product)
+        const inCart = cart.find((cartItem) => cartItem.id === product.id) 
+        console.log('inCart', inCart)
+        if (inCart) {
+          setCart(cart.map((cartItem) => 
+            cartItem.id === product.id ? {...inCart, ammount: inCart.ammount + 1} : cartItem))
+        } else {
+            setCart([...cart, {...product, ammount: 1}])
+        }
+        console.log('Cart:', cart)
+     }
+     const removeFromCart = (product) => {
+        const inCart = cart.find((cartItem) => cartItem.id === product.id) 
+        if(inCart.ammount === 1) {
+            setCart(cart.filter((cartItem) => cartItem.id !== product.id))
+        } else {
+            setCart(cart.map((cartItem) => 
+            cartItem.id === product.id ? {...inCart, ammount: inCart.ammount - 1} : cartItem ))
+        }
+        console.log('item removed', cart)
+     }
+
   return (
     <BrowserRouter>
       <NavBar setToken={setToken} token={token} />
@@ -35,10 +51,10 @@ const Site = ({
         <Route exact path="/" component={Home} />
 
         <Route exact path="/products">
-          <Products products={products} />
+          <Products products={products} cart={cart} setCart={setCart} addToCart={addToCart}/>
         </Route>
         <Route exact path="/products/:productId">
-          <SingleProductPage products={products} />
+          <SingleProductPage products={products} cart={cart} setCart={setCart} addToCart={addToCart}/>
         </Route>
 
         <Route path="/login">
@@ -64,6 +80,8 @@ const Site = ({
             orders={orders}
             cart={cart}
             setCart={setCart}
+            addToCart={addToCart}
+            removeFromCart={removeFromCart}
           />
         </Route>
       </Switch>
